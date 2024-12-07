381736b62cba61738a88eba00b989bd10c633b50
Akar-30 <akar511.30@gmail.com>
2024-12-07 21:26:28 +0300

    refactor Process2 class: update process attributes, implement priority scheduling with heap, and enhance output formatting

--- semester5/os/ass1/Process2.py
+++ semester5/os/ass1/Process2.py
@@ -1,55 +1,107 @@
-from queue import PriorityQueue
-
 class Process:
-    def __init__(self, pid, priority, arrival_time, burst_time):
-        self.pid = pid
+    def __init__(self, processID, priority, arrivalTime, burstTime):
+        self.processID = processID
         self.priority = priority
-        self.arrival_time = arrival_time
-        self.burst_time = burst_time
-        self.remaining_time = burst_time
+        self.arrivalTime = arrivalTime
+        self.burstTime = burstTime
+        self.tempburstTime = burstTime
+        self.responsetime = -1
+        self.outtime = 0
+        self.intime = -1
         self.completion_time = 0
         self.turnaround_time = 0
         self.waiting_time = 0
 
-def preemptive_priority_scheduling(processes):
-    time = 0
-    pq = PriorityQueue()
-    n = len(processes)
-    completed = 0
-    current_index = 0
+def insert(Heap, value, heapsize, currentTime):
+    start = heapsize[0]
+    Heap[heapsize[0]] = value
+    if Heap[heapsize[0]].intime == -1:
+        Heap[heapsize[0]].intime = currentTime[0]
+    heapsize[0] += 1
 
-    while completed < n:
-        while current_index < n and processes[current_index].arrival_time <= time:
-            pq.put((processes[current_index].priority, current_index, processes[current_index]))
-            current_index += 1
+    # Ordering the Heap
+    while start != 0 and Heap[(start - 1) // 2].priority > Heap[start].priority:
+        Heap[(start - 1) // 2], Heap[start] = Heap[start], Heap[(start - 1) // 2]
+        start = (start - 1) // 2
 
-        if not pq.empty():
-            current_process = pq.get()[2]
-            print(f"Time {time}: Processing {current_process.pid}")
-            time += 1
-            current_process.remaining_time -= 1
+def order(Heap, heapsize, start):
+    smallest = start
+    left = 2 * start + 1
+    right = 2 * start + 2
+    if left < heapsize[0] and Heap[left].priority < Heap[smallest].priority:
+        smallest = left
+    if right < heapsize[0] and Heap[right].priority < Heap[smallest].priority:
+        smallest = right
 
-            if current_process.remaining_time == 0:
-                current_process.completion_time = time
-                current_process.turnaround_time = current_process.completion_time - current_process.arrival_time
-                current_process.waiting_time = current_process.turnaround_time - current_process.burst_time
-                completed += 1
-                print(f"Process {current_process.pid} completed at time {time}")
-        else:
-            if current_index >= n:
-                break  # Break the loop if no more processes to add and queue is empty
-            print(f"Time {time}: No process in queue, incrementing time")
-            time += 1
+    # Ordering the Heap
+    if smallest != start:
+        Heap[start], Heap[smallest] = Heap[smallest], Heap[start]
+        order(Heap, heapsize, smallest)
 
-    return processes
+def extract_minimum(Heap, heapsize, currentTime):
+    min_process = Heap[0]
+    if min_process.responsetime == -1:
+        min_process.responsetime = currentTime[0] - min_process.arrivalTime
+    heapsize[0] -= 1
+    Heap[0] = Heap[heapsize[0]]
+    order(Heap, heapsize, 0)
+    return min_process
 
-def print_processes(processes):
-    print("Process ID\tPriority\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time")
-    for process in processes:
-        print(f"{process.pid}\t\t{process.priority}\t\t{process.arrival_time}\t\t{process.burst_time}\t\t{process.completion_time}\t\t{process.turnaround_time}\t\t{process.waiting_time}")
+def scheduling(Heap, array, n, heapsize, currentTime):
+    if heapsize[0] == 0:
+        return
 
-# Example usage with provided processes
-processes = [
+    min_process = extract_minimum(Heap, heapsize, currentTime)
+    min_process.outtime = currentTime[0] + 1
+    min_process.burstTime -= 1
+    print(f"Process ID = {min_process.processID}, Out Time = {min_process.outtime}, Burst Time Left = {min_process.burstTime}")
+
+    if min_process.burstTime > 0:
+        insert(Heap, min_process, heapsize, currentTime)
+    else:
+        min_process.completion_time = min_process.outtime
+        min_process.turnaround_time = min_process.completion_time - min_process.arrivalTime
+        min_process.waiting_time = min_process.turnaround_time - min_process.tempburstTime
+
+    currentTime[0] += 1
+
+def priority(array, n):
+    Heap = [None] * n
+    heapsize = [0]
+    currentTime = [0]
+    total_response_time = 0
+    total_waiting_time = 0
+    total_turnaround_time = 0
+    total_burst_time = 0
+
+    while True:
+        for i in range(n):
+            if array[i].arrivalTime == currentTime[0]:
+                insert(Heap, array[i], heapsize, currentTime)
+
+        if heapsize[0] == 0:
+            break
+
+        scheduling(Heap, array, n, heapsize, currentTime)
+
+    for i in range(n):
+        total_response_time += array[i].responsetime
+        total_waiting_time += array[i].waiting_time
+        total_turnaround_time += array[i].turnaround_time
+        total_burst_time += array[i].tempburstTime
+
+    print(f"Average waiting time = {total_waiting_time / n}")
+    print(f"Average response time = {total_response_time / n}")
+    print(f"Average turn around time = {total_turnaround_time / n}")
+
+    print("\nProcess ID\tPriority\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\tResponse Time")
+    for process in array:
+        print(f"{process.processID}\t\t{process.priority}\t\t{process.arrivalTime}\t\t{process.tempburstTime}\t\t{process.completion_time}\t\t{process.turnaround_time}\t\t{process.waiting_time}\t\t{process.responsetime}")
+
+# Driver code
+if __name__ == "__main__":
+    n = 7
+    a = [
     Process(1, 2, 0, 1),
     Process(2, 6, 1, 7),
     Process(3, 3, 2, 3),
@@ -57,7 +109,5 @@ processes = [
     Process(5, 4, 4, 5),
     Process(6, 10, 5, 15),
     Process(7, 9, 15, 8)
-]
-
-scheduled_processes = preemptive_priority_scheduling(processes)
-print_processes(scheduled_processes)
+    ]
+    priority(a, n)
